d7b83e6ae0ab71f33561ff37bac188c2585c377e
currycan <ansandyzzt@gmail.com>
2022-10-13 14:43:39 +0800

    Update helm.sh

--- apisix/helm.sh
+++ apisix/helm.sh
@@ -2,10 +2,12 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add apisix https://charts.apiseven.com
 helm repo update
 helm install apisix apisix/apisix \
-  --namespace ingress-apisix \
+  --create-namespace --namespace ingress-apisix \
   --set gateway.type=LoadBalancer \
   --set admin.allow.ipList="{0.0.0.0/0}" \
   --set dashboard.enabled=true \
   --set etcd.persistence.storageClass="nfs-storage-class" \
   --set etcd.persistence.size="2Gi" \
   --set ingress-controller.enabled=true
+
+kubectl patch svc hubble-ui -n ingress-apisix -p '{"spec": {"type": "LoadBalancer"}}'
